bd45014b4b080ab99d9dabd6f6be2517a3792477
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2023-10-09 18:48:48 +0000

    Arrays in js

---
+++ basic/06-arrays.js
@@ -0,0 +1,27 @@
+let arr = [1,2,3,4,5,6];
+// console.log(arr[0])
+// console.log(arr);
+// arr.push(7);
+// console.log(arr)
+// arr.pop();
+// arr.unshift(10);
+// arr.shift();
+
+// console.log(arr.includes(9))
+// console.log(arr.indexOf(2))
+
+
+// const newArr = arr.join();
+
+// console.log(arr)
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice or splice
+
+console.log("A",arr);
+const myn1 = arr.slice(1,3)
+console.log(myn1);
+console.log("B",arr);
+
+
---
+++ basic/07-array2.js
@@ -0,0 +1,31 @@
+const mravel_heros = ["thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"];
+
+// mravel_heros.push(dc_heros)
+
+// console.log(mravel_heros);
+// console.log(mravel_heros[3][1]);
+
+// const allHeroes = mravel_heros.concat(dc_heros);
+// console.log(allHeroes)
+
+// const all_new_heros = [...mravel_heros,...dc_heros]
+// console.log(all_new_heros)
+
+// const anotherArr= [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const useable_arr = anotherArr.flat(2);
+// console.log(useable_arr);
+
+// const arr = [1,2,3,4]
+// console.log(Array.isArray("kiran"))
+// console.log(Array.from("kiran"));
+// console.log(Array.isArray(arr))
+
+
+// console.log(Array.from({name:"kiran"}))
+
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1,score2,score3))
