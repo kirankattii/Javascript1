9ce3057ba619ce7c4b842223394c06a88665ee5b
Kiran Katti <139916130+kirankattii@users.noreply.github.com>
2023-10-09 18:48:16 +0000

    stack and heap

---
+++ basic/05-memoryManagement.js
@@ -0,0 +1,25 @@
+
+// memory
+// two types
+
+// Stack(premitive type) =>  we get copy
+
+// Heap memory(non primitive type) => we get reference / original value
+
+
+let name = "kiran katti"
+
+let anotherName = name;
+
+anotherName = "ram"
+
+console.log(name);
+console.log(anotherName);
+
+
+
+
+
+
+
+
